91c0d24b6abd616c48c2e4f513d3eefc44f5491e
alzarei <alireza.aut@gmail.com>
2014-04-27 23:35:25 -0700

    creating project using Maven

---
+++ code-snippets/src/main/java/ca/ubc/ece/azarei/snippets/App.java
@@ -0,0 +1,13 @@
+package ca.ubc.ece.azarei.snippets;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
